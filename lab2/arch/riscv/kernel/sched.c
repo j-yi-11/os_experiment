6c37ae0273e261ef2b6fc2acd3b75321a100c346
菜鸭 <3525472411@qq.com>
2023-10-10 14:51:27 +0800

    更新init task

--- lab2/arch/riscv/kernel/sched.c
+++ lab2/arch/riscv/kernel/sched.c
@@ -28,8 +28,8 @@ void task_init(void) {
   for(int i = 0; i < LAB_TEST_NUM; ++i) {
     // TODO
     // initialize task[i]
-    task[i] = (struct task_struct*)kalloc();
     // get the task_struct based on Kernel_Page and i
+    task[i] = (struct task_struct*)(Kernel_Page + i * PAGE_SIZE); // kalloc()
     // set state = TASK_RUNNING, counter = 0, priority = 5, 
     task[i]->state = TASK_RUNNING;
     task[i]->counter = 0;
